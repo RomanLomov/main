925561acfe20c6533302ba829d3167adbb13d046
RomanLomov <74732253+RomanLomov@users.noreply.github.com>
2022-04-27 21:24:02 +0300

    Create parser.js

---
+++ parser.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const body = document.querySelector('body');
+    let textNodes = [];
+    
+    function recursy (element) { 
+      element.childNodes.forEach(node => {
+      
+        if (node.nodeName.math(/^H\d/)) {
+          const obj = { 
+            header: node.nodeName,
+            content: node.textContent
+          }
+          textNodes.push(obj);
+        } else {
+           recursy(node);
+        }   
+      });  
+    }
+    
+    recursy(body);
